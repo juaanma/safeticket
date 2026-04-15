389f22218007329996c40ac05c30fdfada26e408
jnm <90472682+juaanma@users.noreply.github.com>
2026-04-15 00:27:24 -0300

    Delete fix.js

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let txt = fs.readFileSync('login.html', 'utf8');
-txt = txt.replace(/placeholder=".*?" required/, 'placeholder="********" required');
-fs.writeFileSync('login.html', txt);
-let txt2 = fs.readFileSync('register.html', 'utf8');
-txt2 = txt2.replace(/placeholder=".*?" required/, 'placeholder="********" required');
-fs.writeFileSync('register.html', txt2);
